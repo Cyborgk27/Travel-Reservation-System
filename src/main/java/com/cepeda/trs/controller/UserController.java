72c67fbc18363d1e630919ccb032392be4094f09
Cyborgk27 <slenderstalin@hotmail.com>
2024-07-04 11:27:42 -0500

    feat - add login validator

--- src/main/java/com/cepeda/trs/controller/UserController.java
+++ src/main/java/com/cepeda/trs/controller/UserController.java
@@ -2,6 +2,9 @@ package com.cepeda.trs.controller;
 
 import com.cepeda.trs.model.persistence.interfaces.IUserRepository;
 import com.cepeda.trs.model.persistence.repositories.UserRepository;
+import com.cepeda.utiities.Validators;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
@@ -10,13 +13,23 @@ import javax.swing.JTextField;
  * @author CyborgK27
  */
 public class UserController {
-    private IUserRepository  _UserRepository;
+
+    private IUserRepository _UserRepository;
 
     public UserController() {
         _UserRepository = new UserRepository();
     }
-    
-    public void SignIn(JTextField txtEmail, JPasswordField JpPassword){
-        
+
+    public void SignIn(JTextField txtEmail, JPasswordField pfPassword,
+            JLabel lblEmailValidator, JLabel lblPasswordValidator) {
+        String email = txtEmail.getText();
+        String password = new String(pfPassword.getPassword());
+        if (Validators.validateLogin(email, password)) {
+            Validators.validateEmail(email, lblEmailValidator);
+            Validators.validatePassword(password, lblPasswordValidator);
+            JOptionPane.showMessageDialog(null, "Inicio de sesión con éxito");
+        }else{
+            JOptionPane.showMessageDialog(null, "No dejar campos vacios");
+        }
     }
 }
